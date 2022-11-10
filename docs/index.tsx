62e6797591daf3964e7a712bf240365a67b19112
Erik Pukinskis <erik@snowedin.net>
2022-11-09 21:00:37 -0700

    Add demo in docs of orderable list

--- docs/index.tsx
+++ docs/index.tsx
@@ -1,9 +1,13 @@
 import { DocsApp } from "codedocs"
 import React from "react"
 import { render } from "react-dom"
-import * as Home from "./Home.docs"
+import * as HomeDocs from "./Home.docs"
+import * as UseOrderableListDocs from "~/useOrderableList.docs"
 
 render(
-  <DocsApp logo="React Headless Accessible Hooks" docs={[Home]} />,
+  <DocsApp
+    logo="React Headless Accessible Hooks"
+    docs={[HomeDocs, UseOrderableListDocs]}
+  />,
   document.getElementById("root")
 )
